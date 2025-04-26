0d99bffffa5ef447de2ec1cf4364dbff800bb14e
Chandini624 <gchandini784@gmail.com>
2025-04-26 14:26:08 +0530

    add docter list
    add

--- login/main.js
+++ login/main.js
@@ -1,11 +1,16 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+// Ensure the DOM is fully loaded before running the script
+    document.addEventListener("DOMContentLoaded", () => {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
+    // Add event listener for the signUp button to add the active class
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+    // Add event listener for the signIn button to remove the active class
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
 });
